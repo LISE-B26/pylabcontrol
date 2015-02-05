06550805d09f06e6c0c4100cabf99e581c71fd20
Aaron <akabcenell@g.harvard.edu>
2015-02-05 10:48:01 -0500

    Rewrote PiezoController

--- PiezoController.py
+++ PiezoController.py
@@ -2,96 +2,88 @@
 # Last Update: 2/3/15
 
 import serial
+import time
 
 class MDT693A:
 
-    def __init__(self, port = 7, baudrate=115200, timeout = .1):
+    SYSTEM_OFFSET = 0.6 # volts
+
+    def __init__(self, outputAxis, port = 7, baudrate=115200, timeout = .1):
         # The serial connection should be setup with the following parameters:
         # 8 data bits, No parity bit, 1 stop bit, no hardware
         # handshake. These are all default for Serial and therefore not input
         # below
+
+        if (outputAxis is not 'X' and outputAxis is not 'Y' and outputAxis is not 'Z'):
+            message = 'Piezo Controller Axis not correctly defined; must be either \'X\', \'Y\', or \'Z\''
+            raise ValueError(message)
+
+        self.axis = outputAxis
+
         self.ser = serial.Serial(port = port, baudrate=baudrate, timeout = timeout)
+        self.ser.close()
+        self.manualOffset = -1
+        self.getManualOffset()
 
-    # get the voltage on the x-axis port of the piezo controller
-    def getXVoltage(self):
-        assert self.ser.isOpen()
+    # get the voltage on the port 'axis' of the piezo controller
+    def getVoltage(self):
 
-        self.ser.write('XR?\r')
+        if not self.ser.isOpen():
+            self.ser.open()
+
+        self.ser.write(self.axis + 'R?\r')
         xVoltage = self.ser.readline()[6:-2].strip()
+
+        self.ser.close()
 
         return float(xVoltage)
 
-    # get the voltage on the y-axis port of the piezo controller
-    def getYVoltage(self):
-        assert self.ser.isOpen()
+    # set the voltage on the port 'axis' of the piezo controller
+    def setVoltage(self, voltage):
 
-        self.ser.write('YR?\r')
-        yVoltage = self.ser.readline()[6:-2].strip()
+        if not self.ser.isOpen():
+            self.ser.open()
 
-        return float(yVoltage)
+        #print 'Setting voltage for ' + self.axis + ' axis: ' + str(voltage - self.manualOffset)
+        if (self.manualOffset > voltage):
+            message = 'Voltage to be set is less than manually set voltage; cannot change to desired value.' \
+                      'Manually set voltage: %s V. Voltage desired: %s V.' % (str(self.manualOffset), str(voltage))
+            raise ValueError(message)
 
-    # get the voltage on the z-axis port of the piezo controller
-    def getZVoltage(self):
-        assert self.ser.isOpen()
+        elif (self.SYSTEM_OFFSET > voltage):
+            message = 'The controller has a small bias and cannot go below ' + str(self.SYSTEM_OFFSET) + ' V .'
+            raise ValueError(message)
 
-        self.ser.write('ZR?\r')
-        zVoltage = self.ser.readline()[6:-2].strip()
+        else:
+            self.ser.write('%s%f\r'%(self.axis + 'V', voltage - self.manualOffset))
+            self.ser.readline()
 
-        return float(zVoltage)
+        self.ser.close()
 
-    # set the voltage on the x-axis port of the piezo controller
-    def setXVoltage(self, voltage):
-        assert self.ser.isOpen()
+    # gets the manual offset on the output port 'axis'.
+    # This is an additional voltage added via the physical front panel knob
+    def getManualOffset(self):
 
-        self.ser.write('%s%f\r'%('XV', voltage))
+        if not self.ser.isOpen():
+            self.ser.open()
+
+        curr_voltage = self.getVoltage()
+        self.setSoftwareVoltage(0)
+        self.manualOffset = self.getVoltage() - self.SYSTEM_OFFSET
+        self.setSoftwareVoltage(curr_voltage - self.manualOffset)
+
+        self.ser.close()
+        return self.manualOffset
+
+    def setSoftwareVoltage(self, voltage):
+        if not self.ser.isOpen():
+            self.ser.open()
+
+        self.ser.write('%s%f\r'%(self.axis + 'V', voltage))
         self.ser.readline()
 
-    # set the voltage on the y-axis port of the piezo controller
-    def setYVoltage(self, voltage):
-        assert self.ser.isOpen()
+        self.ser.close()
 
-        self.ser.write('%s%f\r'%('YV', voltage))
-        self.ser.readline()
-
-    # set the voltage on the z-axis port of the piezo controller
-    def setZVoltage(self, voltage):
-        assert self.ser.isOpen()
-
-        self.ser.write('%s%f\r'%('ZV', voltage))
-        self.ser.readline()
-
-    # gets the manual offset on the x axis. This is an additional voltage added to the x axis via the physical front
-    # panel knob
-    def getXManualOffset(self):
-        assert self.ser.isOpen()
-
-        curr_voltage = self.getXVoltage()
-        self.setXVoltage(0);
-        manualOffset = self.getXVoltage()
-        self.setXVoltage(curr_voltage - manualOffset)
-        return manualOffset
-
-    # gets the manual offset on the y axis. This is an additional voltage added to the y axis via the physical front
-    # panel knob
-    def getYManualOffset(self):
-        assert self.ser.isOpen()
-
-        curr_voltage = self.getYVoltage()
-        self.setYVoltage(0);
-        manualOffset = self.getYVoltage()
-        self.setYVoltage(curr_voltage - manualOffset)
-        return manualOffset
-
-    # gets the manual offset on the z axis. This is an additional voltage added to the z axis via the physical front
-    # panel knob
-    def getZManualOffset(self):
-        assert self.ser.isOpen()
-
-        curr_voltage = self.getZVoltage()
-        self.setZVoltage(0);
-        manualOffset = self.getZVoltage()
-        self.setZVoltage(curr_voltage - manualOffset)
-        return manualOffset
 
     # closes the connection with the controller
     def closeConnection(self):
@@ -100,35 +92,44 @@ class MDT693A:
 
 if __name__ == '__main__':
 
-    controller = MDT693A()
-    voltages = [controller.getXVoltage(), controller.getYVoltage(), controller.getZVoltage()]
-    print 'The X voltage is ' + str(voltages[0])
-    print 'The Y voltage is ' + str(voltages[1])
-    print 'The Z voltage is ' + str(voltages[2])
+    xController = MDT693A('X')
+    yController = MDT693A('Y')
+    zController = MDT693A('Z')
+    initialVoltages = [xController.getVoltage(), yController.getVoltage(), zController.getVoltage()]
+    print 'The X voltage is ' + str(initialVoltages[0])
+    print 'The Y voltage is ' + str(initialVoltages[1])
+    print 'The Z voltage is ' + str(initialVoltages[2])
 
-    testVoltages = [5, 10, 15]
+    testVoltages = [5.0, 10.0, 15.0]
     print 'Setting x, y, and z voltages to 5, 10, and 15, respectively...'
-    controller.setXVoltage(testVoltages[0])
-    controller.setYVoltage(testVoltages[1])
-    controller.setZVoltage(testVoltages[2])
+    xController.setVoltage(testVoltages[0])
+    yController.setVoltage(testVoltages[1])
+    zController.setVoltage(testVoltages[2])
 
-    print 'The X voltage is now ' + str(controller.getXVoltage())
-    print 'The Y voltage is now ' + str(controller.getYVoltage())
-    print 'The Z voltage is now ' + str(controller.getZVoltage())
+    print 'The X voltage is now ' + str(xController.getVoltage())
+    print 'The Y voltage is now ' + str(yController.getVoltage())
+    print 'The Z voltage is now ' + str(zController.getVoltage())
 
     print 'Setting Voltages back...'
-    controller.setXVoltage(voltages[0])
-    controller.setYVoltage(voltages[1])
-    controller.setZVoltage(voltages[2])
-    currentVoltages = [controller.getXVoltage(), controller.getYVoltage(), controller.getZVoltage()]
+    xController.setVoltage(initialVoltages[0])
+    xController.setVoltage(initialVoltages[1])
+    yController.setVoltage(initialVoltages[2])
+    #time.sleep(10)
+    currentVoltages = [xController.getVoltage(), yController.getVoltage(), zController.getVoltage()]
+    print currentVoltages
 
-    if abs(voltages[0] - currentVoltages[0]) < 1 and abs(voltages[1] - currentVoltages[1]) < 1 and abs(voltages[2] - currentVoltages[2]) < 1:
+    if abs(initialVoltages[0] - currentVoltages[0]) < 1 and abs(initialVoltages[1] - currentVoltages[1]) < 1 and abs(initialVoltages[2] - currentVoltages[2]) < 1:
         print 'Successfully set voltages back to original values'
     else:
-        print 'Was not able to set voltages back to original values'
+        print 'Was not able to set voltages back to original values. /_\X = %f, /_\Y = %f, /_\Z = %f' % (initialVoltages[0] - currentVoltages[0], initialVoltages[1] - currentVoltages[1], initialVoltages[2] - currentVoltages[2])
 
-    print 'The x manual offset is ' + str(controller.getXManualOffset())
-    print 'The y manual offset is ' + str(controller.getYManualOffset())
-    print 'The y manual offset is ' + str(controller.getZManualOffset())
+    print 'The X manual offset is ' + str(xController.getManualOffset())
+    print 'The Y manual offset is ' + str(yController.getManualOffset())
+    print 'The Z manual offset is ' + str(zController.getManualOffset())
 
-    controller.closeConnection()
+    for i in xrange(80):
+        yController.setVoltage(i+1)
+        print 'Set the voltage to %s, read voltage %s' % (str(i+1), str(yController.getVoltage()))
+
+
+    yController.setVoltage(10)
