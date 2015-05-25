Fixed scaling bugs

functions/Focusing.py
@@ -64,7 +64,7 @@ class Focus:
             axes = canvas.axes
         # plots junk data to initialize lines used later
         dat=[-1,0]
-        dat2 = [0,1]
+        dat2 = [0,0]
         datline,fitline = axes.plot(dat,dat2,dat,dat2)
         axes.set_xlim([minV-1,maxV+1])
         #axes.set_ylim([0,10])
@@ -106,7 +106,7 @@ class Focus:
             return scipy.optimize.curve_fit(cls.gaussian, x, y, p0=[1, mean, sigma, c])
         except RuntimeError:
             print('Gaussian fit failed. Setting piezo to mean of input range')
-            return (-1,-1,-1),'Ignore'
+            return (-1,-1,-1,-1),'Ignore'
 
     # defines a gaussian for use in the fitting routine
     @staticmethod

scripts/scan_focus_taking_images.py
@@ -9,7 +9,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from functions.save_array_to_disk import *
-# set variables
+
+# set variables =============================================
+# ===========================================================
 
 axis = 'X'
 xVmin,xVmax,xPts,yVmin,yVmax,yPts = -0.4, 0.4, 120.,  -0.4, 0.4, 120.
@@ -17,20 +19,23 @@ timePerPt = 0.001
 
 dirpath = 'Z:/Lab/Cantilever/Measurements/150521_FindSiNi_Beams/ScanZ_and _take_image/'
 
-tag = 'Si3Ni4'
+tag = 'Si3Ni4_withGlassSlide'
 
-voltages = np.arange(10,70,10)
+voltages = np.arange(10,90,10)
 
-#run script
+#run script =================================================
+# ===========================================================
 
 for counter, voltage in enumerate(voltages):
+    # set focus
+    xController = pc.MDT693A('X')
+    xController.setVoltage(voltage)
+
     # acquire data
     scanner = GalvoScan.ScanNV(xVmin,xVmax,xPts,yVmin,yVmax,yPts,timePerPt, canvas = None)
     imageData = scanner.scan()
 
-    # change focus
-    xController = pc.MDT693A('X')
-    xController.setVoltage(voltage)
+
 
     # plot image
     fig = plt.figure()