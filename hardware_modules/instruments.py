65b195f9d65343c88d7ef1886d6f87469908bb4b
Jan Gieseler <Jan.Gieseler@hotmail.com>
2016-03-08 12:23:18 -0500

    JG:parameter class finished and tested, also generic instrument class finsished

--- hardware_modules/instruments.py
+++ hardware_modules/instruments.py
@@ -51,6 +51,9 @@ class Parameter(object):
 
         assert self.isvalid(value), "value is not if valid type"
 
+    def __repr__(self):
+        return str(self.dict)
+
     @property
     def dict(self):
         return self._data
@@ -76,6 +79,17 @@ class Parameter(object):
         else:
             raise TypeError('Wrong type! \
                              Type should be .. improve msg here')
+    @property
+    def info(self):
+        return self._data['info']
+    @info.setter
+    def info(self, value):
+        if isinstance(value, str):
+            self._data.update({'info' : value})
+        else:
+            raise TypeError('Wrong type! \
+                             info should be a string')
+
     def isvalid(self, value):
         '''
         checks if value is a valid parameter value
@@ -95,60 +109,161 @@ class Parameter(object):
 
         return valid
 
+    @property
+    def valid_values(self):
+        return self._data['valid_values']
+
+    def __eq__(self, other):
+        '''
+        checks if two parameters have the same valid_values and name
+        :param other:
+        :return:
+        '''
+
+        is_equal = True
+        if self.name != other.name:
+            is_equal = False
+        if self.valid_values != other.valid_values:
+            is_equal = False
+        return is_equal
+
+    def update(self, other):
+        if self == other:
+            self.value = other.value
+            self.info = other.info
+        else:
+            raise TypeError('Parameters are not of the same type! \
+                             They should have the same name and same valid values')
+
 class Instrument(object):
     '''
     generic instrument class
     '''
-    def __init__(self, config = None):
+    def __init__(self, parameter_list = []):
 
-        if config ==None:
-            config = self.config_default
+        assert isinstance(parameter_list, list), 'parameter_list has to be a list'
 
-        for key, val in config:
-            self.set_parameter(key, val)
+        if parameter_list == []:
+            self._parameter_list = self.parameter_list_default
 
+
+    def __str__(self):
+        output_string = 'class type {:s}\n'.format(self.__class__.__name__)
+        for parameter in self.parameter_list:
+            output_string += '{:s}:\t {:s}\n'.format(parameter.name, str(parameter.value))
+        return output_string
+
+    @property
 
     @property
     def config(self):
         '''
         returns the configuration of the instrument as a dictionary
-        :return:
+        :return: dictionary with entries name and value
         '''
-        return self._config
+        # build dictionary
+        config = {}
+        for p in self._parameter_list:
+            config.update({p['name'] : p['value']})
+
+        return config
 
 
     @property
-    def config_default(self):
+    def parameter_list_default(self):
         '''
-        returns the default configuration of the instrument as a dictionary
+        returns the default parameter_list of the instrument
         :return:
         '''
+        parameter_list_default = []
+        return parameter_list_default
 
-        return self._config_default
+    @property
+    def parameter_list(self):
+        return self._parameter_list
 
-    def set_parameter(self, parameter):
-        if parameter in self._parameter_list:
-            self._config
+    def update_parameter_list(self, parameter_list_new):
+        '''
+        updates the parameters if they exist
+        :param parameter_list_new:
+        :return:
+        '''
+        for parameter in parameter_list_new:
+            # get index of parameter in default list
+            index = [i for i, p in enumerate(self.parameter_list_default) if p == parameter]
+            if len(index)>1:
+                raise TypeError('Error: Dublicate parameter in default list')
+            elif len(index)==1:
+                self._parameter_list[index[0]].update(parameter)
+
+
+
+class ZI_Sweeper(Instrument):
+    def __init__(self):
+        super(ZI_Sweeper, self).__init__()
+    @property
+    def parameter_list_default(self):
+        '''
+        returns the default parameter_list of the instrument
+        :return:
+        '''
+        parameter_list_default = []
+        return parameter_list_default
+
+
+class Instrument_Dummy(Instrument):
+        '''
+        dummy instrument class, just to see how the creation of a new instrument works
+        '''
+        def __init__(self):
+            super(Instrument_Dummy, self).__init__()
+        @property
+        def parameter_list_default(self):
+            '''
+            returns the default parameter_list of the instrument
+            :return:
+            '''
+            parameter_list_default = [
+                Parameter('parameter 1', 0),
+                Parameter('parameter 2', 2.0)
+            ]
+            return parameter_list_default
 
 
 def test_parameter():
     passed =True
 
     try:
-        p = Parameter('param', 0.0)
-        p = Parameter('param', 0, int, 'test int')
-        p = Parameter('param', 0.0, (int, float), 'test tupple')
-        p = Parameter('param', 0.0, [0.0, 0.1], 'test list')
+
+        p1 = Parameter('param', 0.0)
+        p2 = Parameter('param', 0, int, 'test int')
+        p3 = Parameter('param', 0.0, (int, float), 'test tupple')
+        p4 = Parameter('param', 0.0, [0.0, 0.1], 'test list')
+        print('passed single parameter test')
+
+        p_nested = Parameter('param nested', p1, None, 'test list')
+        p_nested = Parameter('param nested', p4, [p1,p4], 'test list')
+        print('passed nested parameter test')
     except:
         passed =False
 
     return passed
 
-if __name__ == '__main__':
-    # print(test_parameter())
-    p1 = Parameter('param1', 0.0, (int, float), 'test tupple')
-    p2 = Parameter('param1', 'X', str, 'test X')
-    parameter_list = [p1,p2]
+def test_intrument():
 
-    for p in parameter_list:
-        print(p.name)
+
+
+    inst = Instrument_Dummy()
+    p_new = Parameter('parameter 1', 1)
+    inst.update_parameter_list([p_new])
+
+
+    print("instrument test passed")
+if __name__ == '__main__':
+
+    # test_parameter()
+    # test_intrument()
+    inst = Instrument_Dummy()
+    p_new = Parameter('parameter 1', 1)
+    inst.update_parameter_list([p_new])
+    print(inst)
