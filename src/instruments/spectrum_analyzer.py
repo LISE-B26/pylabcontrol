77096fdc325149b9a95dedf3c68d95bbe68de64b
Arthur <arthur.safira@gmail.com>
2016-03-17 19:18:57 -0400

    AS: Got to a point in the testing where I'm not sure what I did wrong --- will come in tomorrow to discuss. TypeError: 'instancemethod' object is not iterable in the Parameter class at the point 'for p in self._parameters'', but since Jan fixed the parameter class, makes sense to wait until tomorrow.

--- src/instruments/spectrum_analyzer.py
+++ src/instruments/spectrum_analyzer.py
@@ -1,11 +1,6 @@
 import visa
 from src.core import Instrument, Parameter
 
-rm = visa.ResourceManager()
-print rm.list_resources()
-inst = rm.open_resource('USB0::0x0957::0xFFEF::CN0323B356::INSTR')
-print(inst.query('*IDN?\n'))
-
 
 class SpectrumAnalyzer(Instrument):
     """
@@ -15,22 +10,21 @@ class SpectrumAnalyzer(Instrument):
     INSTRUMENT_IDENTIFIER = 'Keysight Technologies,N9320B,CN0323B356,0B.03.58'
     # String returned by spectrum analyzer upon querying it with '*IDN?'
 
-    def __init__(self, name, parameter_list=[]):
+    def __init__(self, name='SpectrumAnalyzer', parameter_list=[]):
         super(SpectrumAnalyzer, self).__init__(name, parameter_list)
         self.rm = visa.ResourceManager()
-        self.spec_anal = rm.open_resource(self.visa_resource)
+        self.spec_anal = self.rm.open_resource(self.visa_resource)
 
     def parameters_default(self):
 
         parameter_list_default = [
-            Parameter('visa_resource', 'USB0::0x0957::0xFFEF::CN0323B356::INSTR', str,
+            Parameter('visa_resource', 'USB0::0x0957::0xFFEF::CN0323B356::INSTR', (str),
                       'pyVisa instrument identifier, ''to make a connection using the pyVisa package.'),
-            Parameter('start_frequency', 1.5e9, int, 'start frequency of spectrum analyzer frequency range'),
-            Parameter('stop_frequency', 3e9, int, 'stop frequency of spectrum analyzer frequency range')
+            Parameter('start_frequency', 1.5e9, (int), 'start frequency of spectrum analyzer frequency range'),
+            Parameter('stop_frequency', 3e9, (int), 'stop frequency of spectrum analyzer frequency range')
         ]
 
         return parameter_list_default
-
 
     def is_connected(self):
         identification = self.spec_anal.query('*IDN?\n')
