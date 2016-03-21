aec94d63d1590db917f61ba48c63cc757aa8b569
Jan Gieseler <Jan.Gieseler@hotmail.com>
2016-03-21 11:33:43 -0400

    changed test.py back to previous version since I didn't merge it properly in the branch merge

--- src/core/test.py
+++ src/core/test.py
@@ -1,34 +1,61 @@
+from unittest import TestCase
+
+import sip
+sip.setapi('QVariant', 2)
 from PyQt4 import QtCore, QtGui
 import sys
-
+from src.core.qt_widgets import B26QTreeItem,B26QTreeWidget
+from src.core import Parameter
 
 class UI(QtGui.QMainWindow):
 
-        def __init__(self, parent=None ):
+    def __init__(self, parent=None ):
 
-            ## Init:
-            super(UI, self).__init__( parent )
+        ## Init:
+        super(UI, self).__init__( parent )
 
-            # ----------------
-            # Create Simple UI with QTreeWidget
-            # ----------------
-            self.centralwidget = QtGui.QWidget(self)
-            self.verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
-            self.treeWidget = QtGui.QTreeWidget(self.centralwidget)
-            self.verticalLayout.addWidget(self.treeWidget)
-            self.setCentralWidget(self.centralwidget)
+        # ----------------
+        # Create Simple UI with QTreeWidget
+        # ----------------
+        self.centralwidget = QtGui.QWidget(self)
+        self.verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
 
-            # ----------------
-            # Set TreeWidget Headers
-            # ----------------
-            HEADERS = ( "parameter", "value" )
-            self.treeWidget.setColumnCount( len(HEADERS) )
-            self.treeWidget.setHeaderLabels( HEADERS )
+        self.parameters = Parameter([
+            Parameter('test1', 0, int, 'test parameter (int)'),
+            Parameter('test2' ,
+                      [Parameter('test2_1', 'string', str, 'test parameter (str)'),
+                       Parameter('test2_2', 0.0, float, 'test parameter (float)'),
+                       Parameter('test2_3', 'a', ['a', 'b', 'c'], 'test parameter (list)'),
+                       Parameter('test2_4', False, bool, 'test parameter (bool)')
+                       ]),
+            Parameter('test3', 'a', ['a', 'b', 'c'], 'test parameter (list)'),
+            Parameter('test4', False, bool, 'test parameter (bool)')
+        ])
 
-            # ----------------
-            # Add Custom QTreeWidgetItem
-            # ----------------
+        self.treeWidget = B26QTreeWidget(self.centralwidget, self.parameters)
 
+        # self.treeWidget = QtGui.QTreeWidget(self.centralwidget)
+        self.verticalLayout.addWidget(self.treeWidget)
+        self.setCentralWidget(self.centralwidget)
+
+        # ----------------
+        # Set TreeWidget Headers
+        # ----------------
+        HEADERS = ( "parameter", "value" )
+        self.treeWidget.setColumnCount( len(HEADERS) )
+        self.treeWidget.setHeaderLabels( HEADERS )
+
+
+        # ----------------
+        # Add Custom QTreeWidgetItem
+        # ----------------
+        self.treeWidget.itemChanged.connect(lambda: self.update_parameters(self.treeWidget, self.parameters))
+
+
+
+    def update_parameters(self, tree, parameters):
+
+        print('sss', tree.currentItem().text(0), tree.currentItem().valid_values)
 
             # my_instruments = [
             #     # Instrument_Dummy('inst dummy 1'),
@@ -56,4 +83,5 @@ if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     ex = UI()
     ex.show()
+    ex.raise_()
     sys.exit(app.exec_())
