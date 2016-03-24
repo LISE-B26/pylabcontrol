5840e19724c99843ac15e20889fd12062b7d7c27
Jan Gieseler <jan.gieseler@hotmail.com>
2016-03-24 16:07:35 -0400

    updated __init__ with sip import

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,10 +1,11 @@
 # some usefull superclasses for the PythonLab project
+import sip
+sip.setapi('QVariant', 2)# s
 from parameter import Parameter
 from instruments import Instrument
 from scripts import Script
-from qt_b26_widgets import B26QTreeItem
 from loading import load_probes, load_scripts, load_instruments
-
+from qt_b26_widgets import B26QTreeItem
 __all__ = ['Instrument', 'Parameter']
 
 
