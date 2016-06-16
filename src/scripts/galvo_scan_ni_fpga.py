closes #24: - loop that writes to DMA still waiting for too many samples. previously I would keep the acq. time fixed and send all the measurements to the host which would then do the averaging. Now I just send the averaged value to the host, thus I send less samples.

src/scripts/galvo_scan_ni_fpga.py
@@ -116,7 +116,7 @@ class GalvoScanNIFpga(Script, QThread):
 
 
 
-                self.data['image_data'][i] = deepcopy(line_data['signal'])
+                self.data['image_data'][i] = deepcopy(line_data['signal'])/1e3
 
                 # set the remaining values to the mean so that we get a good contrast while plotting
                 mean_value = np.mean(self.data['image_data'][0:i])
@@ -132,7 +132,7 @@ class GalvoScanNIFpga(Script, QThread):
                 self.updateProgress.emit(progress)
                 t1 = t2
                 # uncomment following line to show some diagnostic values
-                # print_diagnostics()
+                print_diagnostics()
             else:
 
                 time_sleep = time_per_line_s - elapsed_time.total_seconds()