ab5ec42aad0a30be6c706c3c0c7c886617e373ae
asafira <arthur.safira@gmail.com>
2016-07-29 15:52:39 -0400

    Deleted README.rst

--- src/gui/load_dialog.py
+++ src/gui/load_dialog.py
@@ -6,6 +6,26 @@
 #
 # WARNING! All changes made in this file will be lost!
 
+"""
+    This file is part of PyLabControl, software for laboratory equipment control for scientific experiments.
+    Copyright (C) <2016>  Arthur Safira, Jan Gieseler, Aaron Kabcenell
+
+    Foobar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+
 from PyQt4 import QtCore, QtGui
 
 try:
