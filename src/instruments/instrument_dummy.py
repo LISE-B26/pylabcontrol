b388673fb6698a4bf6dacef1fa54376bf136b77b
Arthur <arthur.safira@gmail.com>
2016-08-19 00:03:49 -0400

    Fixed the license info at the top of the code to not be in docstring format.

--- src/instruments/instrument_dummy.py
+++ src/instruments/instrument_dummy.py
@@ -1,22 +1,22 @@
-"""
-    This file is part of PyLabControl, software for laboratory equipment control for scientific experiments.
-    Copyright (C) <2016>  Arthur Safira, Jan Gieseler, Aaron Kabcenell
+
+# This file is part of PyLabControl, software for laboratory equipment control for scientific experiments.
+# Copyright (C) <2016>  Arthur Safira, Jan Gieseler, Aaron Kabcenell
+#
+#
+# PyLabControl is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyLabControl is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyLabControl.  If not, see <http://www.gnu.org/licenses/>.
 
 
-    PyLabControl is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    PyLabControl is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with PyLabControl.  If not, see <http://www.gnu.org/licenses/>.
-
-"""
 from PyLabControl.src.core import Instrument, Parameter
 from PyQt4.QtCore import QThread
 import random, time
