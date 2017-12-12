dbf1f5f1a4c6786e3e05f65ee5f55a1141606442
akabcenell <akabcenell@g.harvard.edu>
2017-12-12 18:54:24 -0500

    Manual_fitting updated to allow feeding forward of previous good fit values, and fully commented

--- src/gui/manual_fitting.py
+++ src/gui/manual_fitting.py
@@ -22,11 +22,20 @@ import pandas as pd
 Ui_MainWindow, QMainWindow = loadUiType('manual_fitting_window.ui') # with this we don't have to convert the .ui file into a python file!
 
 class FittingWindow(QMainWindow, Ui_MainWindow):
+    """
+    Class defining main gui window for doing manual fitting
+
+    QMainWindow: Qt object returned from loadUiType
+    UI_MainWindow: Ui reference returned from loadUiType
+    """
     def __init__(self):
         QMainWindow.__init__(self)
         self.setupUi(self)
 
         def create_figures():
+            """
+            Sets up figures in GUI
+            """
             self.matplotlibwidget = MatplotlibWidget(self.plot)
             sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
             sizePolicy.setHorizontalStretch(0)
@@ -41,6 +50,9 @@ class FittingWindow(QMainWindow, Ui_MainWindow):
             self.peak_vals = None
 
         def setup_connections():
+            """
+            Sets up all connections between buttons and plots in the gui, and their corresponding backend functions
+            """
             self.btn_fit.clicked.connect(self.btn_clicked)
             self.btn_clear.clicked.connect(self.btn_clicked)
             self.btn_next.clicked.connect(self.btn_clicked)
@@ -55,6 +67,10 @@ class FittingWindow(QMainWindow, Ui_MainWindow):
         setup_connections()
 
     def btn_clicked(self):
+        """
+        Adds the appropriate value to the queue used for communication with the fitting routine thread when a given
+        button is clicked.
+        """
         sender = self.sender()
         if sender is self.btn_run:
             self.start_fitting()
@@ -74,6 +90,11 @@ class FittingWindow(QMainWindow, Ui_MainWindow):
             self.queue.put('skip')
 
     def plot_clicked(self, mouse_event):
+        """
+        When a click is registered on the plot, appends the click location to self.peak_vals and plots a red dot in the
+        click location
+        :param mouse_event: a mouse event object for the click
+        """
         if type(self.peak_vals) is list:
             self.peak_vals.append([mouse_event.xdata, mouse_event.ydata])
             axes = self.matplotlibwidget.axes
@@ -89,10 +110,24 @@ class FittingWindow(QMainWindow, Ui_MainWindow):
             self.matplotlibwidget.draw()
 
     class do_fit(QObject):
+        """
+        This class does all of the work of the manual fitting, including loading and saving data, processing input, and
+        displaying output to the plots. It is implemented as a separate class so that it can be easily moved to a second
+        thread. If it is on the same thread as the gui, you can't both run this code and still interact with the gui.
+        Thus, the second thread allows both to occur simultaneously.
+        """
         finished = pyqtSignal()  # signals the end of the script
         status = pyqtSignal(str) # sends messages to update the statusbar
 
         def __init__(self, filepath, plotwidget, queue, peak_vals, peak_locs):
+            """
+            Initializes the fit class
+            :param filepath: path to file containing the ESR data to fit
+            :param plotwidget: reference to plotwidget passed from gui
+            :param queue: thread-safe queue used for gui-class communication across threads
+            :param peak_vals: peak locations manually selected in gui
+            :param peak_locs: pointer to textbox that will contain locations of fit peaks
+            """
             QObject.__init__(self)
             self.filepath = filepath
             self.plotwidget = plotwidget
@@ -101,6 +136,15 @@ class FittingWindow(QMainWindow, Ui_MainWindow):
             self.peak_locs = peak_locs
 
         def n_lorentzian(self, x, offset, *peak_params):
+            """
+            Defines a lorentzian with n peaks
+            :param x: independent variable
+            :param offset: offset of lorentzians
+            :param peak_params: this is a flattened array of length 3n with the format [widths amplitudes centers], that
+                                is the first n values are the n peak widths, the next n are the n amplitudes, and the
+                                last n are the n centers
+            :return: the output of the lorentzian for each input value x
+            """
             value = 0
             width_array = peak_params[:len(peak_params)/3]
             amplitude_array = peak_params[len(peak_params)/3:2*len(peak_params)/3]
@@ -110,29 +154,48 @@ class FittingWindow(QMainWindow, Ui_MainWindow):
             value += offset
             return value
 
-                # return np.sum((-1/ ((x - center_array[0]) ** 2 + (.5 * width) ** 2))) + offset
-            # return np.sum((-(np.array(amplitude_array) * (.5 * width) ** 2) / ((x - np.array(center_array)) ** 2 + (.5 * width) ** 2))) + offset
-
         def fit_n_lorentzian(self, x, y, fit_start_params=None):
+            """
+            uses scipy.optimize.curve_fit to fit an n-peak lorentzian to the data, where the number of peaks is
+            determined by the number of peaks given as starting parameters for the fit
+            :param x:
+            :param y:
+            :param fit_start_params: this is a flattened array of length 3n + 1 with the format
+                                [offset widths amplitudes centers], that is the first value is hte offset, the next n
+                                values are the n peak widths, the next n are the n amplitudes, and the last n are the n
+                                centers
+            :return: optimized fit values, in the same format as fit_start_params
+            """
             popt, _ = scipy.optimize.curve_fit(self.n_lorentzian, x, y, fit_start_params)
             return popt
 
         def save(self):
+            """
+            Saves the fit data to the data filepath + 'data-manual.csv'. The saving format has one line corresponding to
+            each peak, with the corresponding nv number, fit center, width, and amplitude, and manual center and
+            amplitude included
+            """
             save_path = os.path.join(self.filepath, 'data-manual.csv')
-            df = pd.DataFrame(self.fits)
-            df.to_csv(save_path, index = False)
+            self.fits.to_csv(save_path, index = False)
 
         def load_fitdata(self):
+            """
+            Tries to load and return the previous results of save, or if this file has not been previously analyzed,
+            returns a DataFrame with the correct headers and no data
+            :return: the loaded (or new) pandas DataFrame
+            """
             load_path = os.path.join(self.filepath, 'data-manual.csv')
-            fits = {'nv_id': [], 'peak_id': [], 'offset': [], 'fit_center': [], 'fit_amplitude': [],
-                         'fit_width': [], 'manual_center': [], 'manual_height': []}
             if os.path.exists(load_path):
-                dffits = pd.read_csv(load_path)
-                for key in list(dffits):
-                    fits[key] = dffits[key].tolist()
+                fits = pd.read_csv(load_path)
+            else:
+                fits = pd.DataFrame({'nv_id': [], 'peak_id': [], 'offset': [], 'fit_center': [], 'fit_amplitude': [],
+                        'fit_width': [], 'manual_center': [], 'manual_height': []})
             return fits
 
         def run(self):
+            """
+            Code to run fitting routine. Should be run in a separate thread from the gui.
+            """
             esr_folders = glob.glob(os.path.join(self.filepath, './data_subscripts/*esr*'))
 
             data_array = []
@@ -145,6 +208,8 @@ class FittingWindow(QMainWindow, Ui_MainWindow):
 
             self.status.emit('executing manual fitting')
             index = 0
+            self.last_good = []
+            self.initial_fit = False
             # for data in data_array:
             while index < len(data_array):
                 data = data_array[index]
@@ -152,17 +217,19 @@ class FittingWindow(QMainWindow, Ui_MainWindow):
                 self.status.emit('executing manual fitting NV #' + str(index))
                 self.plotwidget.axes.clear()
                 self.plotwidget.axes.plot(data['frequency'], data['data'])
-                if index in self.fits['nv_id']:
-                    fitdf = pd.DataFrame(self.fits)
-                    fitdf = fitdf.loc[(fitdf['nv_id'] == index)]
+                if index in self.fits['nv_id'].values:
+                    fitdf = self.fits.loc[(self.fits['nv_id'] == index)]
                     offset = fitdf['offset'].as_matrix()[0]
                     centers = fitdf['fit_center'].as_matrix()
                     amplitudes = fitdf['fit_amplitude'].as_matrix()
                     widths = fitdf['fit_width'].as_matrix()
                     fit_params = np.concatenate((np.concatenate((widths, amplitudes)), centers))
                     self.plotwidget.axes.plot(data['frequency'], self.n_lorentzian(data['frequency'], *np.concatenate(([offset], fit_params))))
-                    self.plotwidget.draw()
                 self.plotwidget.draw()
+
+                if not self.last_good == []:
+                    self.initial_fit = True
+                    self.queue.put('fit')
 
                 while(True):
                     if self.queue.empty():
@@ -170,37 +237,39 @@ class FittingWindow(QMainWindow, Ui_MainWindow):
                     else:
                         value = self.queue.get()
                         if value == 'next':
-                            # disabled for feedforward
                             while not self.peak_vals == []:
-                                self.peak_vals.pop(-1)
+                                self.last_good.append(self.peak_vals.pop(-1))
                             if self.single_fit:
-                                to_delete = np.where(np.array(self.fits['nv_id']) == index)
-                                print('to_delete', to_delete)
-                                print(self.fits['nv_id'])
-                                print(index)
-                                for val in to_delete[0][::-1]:
-                                    for key in self.fits.keys():
-                                        del self.fits[key][val]
+                                to_delete = np.where(self.fits['nv_id'].values == index)
+                                # print(self.fits[to_delete])
+                                self.fits = self.fits.drop(self.fits.index[to_delete])
+                                # for val in to_delete[0][::-1]:
+                                #     for key in self.fits.keys():
+                                #         del self.fits[key][val]
                                 for peak in self.single_fit:
-                                    for key in peak.keys():
-                                        self.fits[key].append(peak[key])
+                                    self.fits = self.fits.append(pd.DataFrame(peak))
 
                             index += 1
                             self.status.emit('saving')
                             self.save()
                             break
                         elif value == 'clear':
+                            self.last_good = []
                             self.plotwidget.axes.clear()
                             self.plotwidget.axes.plot(data['frequency'], data['data'])
                             self.plotwidget.draw()
                         elif value == 'fit':
-                            if len(self.peak_vals) > 1:
-                                centers, heights = zip(*self.peak_vals)
+                            if self.initial_fit:
+                                input = self.last_good
+                            else:
+                                input = self.peak_vals
+                            if len(input) > 1:
+                                centers, heights = zip(*input)
                                 widths = 1e7 * np.ones(len(heights))
-                            elif len(self.peak_vals) == 1:
-                                centers, heights = self.peak_vals[0]
+                            elif len(input) == 1:
+                                centers, heights = input[0]
                                 widths = 1e7
-                            elif len(self.peak_vals) == 0:
+                            elif len(input) == 0:
                                 self.single_fit = None
                                 self.peak_locs.setText('No Peak')
                                 self.plotwidget.axes.plot(data['frequency'],np.repeat(np.mean(data['data']), len(data['frequency'])))
@@ -208,16 +277,18 @@ class FittingWindow(QMainWindow, Ui_MainWindow):
                                 continue
                             offset = np.mean(data['data'])
                             amplitudes = offset-np.array(heights)
-                            if len(self.peak_vals) > 1:
+                            if len(input) > 1:
                                 fit_start_params = [[offset], np.concatenate((widths, amplitudes, centers))]
                                 fit_start_params = [y for x in fit_start_params for y in x]
-                            elif len(self.peak_vals) == 1:
+                            elif len(input) == 1:
                                 fit_start_params = [offset, widths, amplitudes, centers]
                             try:
                                 popt = self.fit_n_lorentzian(data['frequency'], data['data'], fit_start_params = fit_start_params)
                             except RuntimeError:
                                 print('fit failed, optimal parameters not found')
                                 break
+                            self.plotwidget.axes.clear()
+                            self.plotwidget.axes.plot(data['frequency'], data['data'])
                             self.plotwidget.axes.plot(data['frequency'], self.n_lorentzian(data['frequency'], *popt))
                             self.plotwidget.draw()
                             params = popt[1:]
@@ -228,9 +299,10 @@ class FittingWindow(QMainWindow, Ui_MainWindow):
                             self.single_fit = []
                             peak_index = 0
                             for position in positions:
-                                self.single_fit.append({'nv_id': index, 'peak_id': peak_index, 'offset': popt[0], 'fit_center': position[0], 'fit_amplitude': position[1], 'fit_width': position[2], 'manual_center': self.peak_vals[peak_index][0], 'manual_height': self.peak_vals[peak_index][1]})
+                                self.single_fit.append({'nv_id': [index], 'peak_id': [peak_index], 'offset': [popt[0]], 'fit_center': [position[0]], 'fit_amplitude': [position[1]], 'fit_width': [position[2]], 'manual_center': [input[peak_index][0]], 'manual_height': [input[peak_index][1]]})
                                 peak_index += 1
                             self.peak_locs.setText('Peak Positions: ' + str(center_array))
+                            self.initial_fit = False
                         elif value == 'prev':
                             index -= 1
                             break
@@ -244,11 +316,17 @@ class FittingWindow(QMainWindow, Ui_MainWindow):
             self.finished.emit()
             self.status.emit('dataset finished')
 
-
     def update_status(self, str):
+        """
+        Updates the gui statusbar with the given string
+        :param str: string to set the statusbar
+        """
         self.statusbar.showMessage(str)
 
     def start_fitting(self):
+        """
+        Launches the fitting routine on another thread
+        """
         self.queue = Queue.Queue()
         self.peak_vals = []
         self.fit_thread = QThread() #must be assigned as an instance variable, not local, as otherwise thread is garbage
@@ -262,7 +340,7 @@ class FittingWindow(QMainWindow, Ui_MainWindow):
 
     def open_file_dialog(self):
         """
-        opens a file dialog to get the path to a file and
+        opens a file dialog to get the path to a file and sets the data_filepath entry area to that fliepath
         """
         dialog = QtGui.QFileDialog
         filename = dialog.getExistingDirectory(self, 'Select a file:', self.data_filepath.text())
